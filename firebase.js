e3db3d7712a9792aa04a517a8b866d4afe98aa97
pay1995 <payumicctv1995@gmail.com>
2025-12-09 07:37:38 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,8 @@
-// ---------------------------
+// ------------------------------
 // Firebase CONFIG
-// ---------------------------
+// ------------------------------
 const firebaseConfig = {
-    apiKey: "AIzaSyCLdTI_qgNPKa9JgQ1qNVYGy-f1o-WYUN8",
+    apiKey: "AIzaSyCLdTI_qgNPkA9JgQ1qNVYGy-f1o-WYUN8",
     authDomain: "paynet-billing.firebaseapp.com",
     projectId: "paynet-billing",
     storageBucket: "paynet-billing.firebasestorage.app",
@@ -10,12 +10,11 @@ const firebaseConfig = {
     appId: "1:1073884574562:web:a702b421fabfe1679bb23b"
 };
 
-// ---------------------------
+// ------------------------------
 // Firebase Init
-// ---------------------------
-import { initializeApp } 
-    from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+// ------------------------------
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const app = initializeApp(firebaseConfig);
-
-export { app };
+export const db = getFirestore(app);
